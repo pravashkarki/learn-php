1bc674e3b356f9121f9e2d5657fda9cd3178c99a
Pravash Karki <pravash.karki@heraldcollege.edu.np>
2022-12-01 22:02:18 +0545

    creating variables

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+<?php
+    $greeting = "Hello";
+    // echo $greeting . " " . "Pravash";
+    echo "$greeting Everyone!";
+?>
+</body>
+</html>
