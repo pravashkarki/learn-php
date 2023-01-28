44bbba3e00986852d4a1c766837cda5ddb8ef9fd
pravashkarki <kpravash@gmail.com>
2023-01-29 03:31:39 +0545

    update: organize code

--- lambda-functions.php
+++ lambda-functions.php
@@ -1,3 +1,49 @@
+<?php
+$books = [
+    [
+        'name' => 'Hail Mary',
+        'releaseYear' => '2015',
+        'author' => 'Pravash',
+        'purchaseUrl' => 'http://example.com'
+
+    ],
+    [
+        'name' => 'Do Androids',
+        'releaseYear' => '2018',
+        'author' => 'Philip',
+        'purchaseUrl' => 'http://example.com'
+
+    ],
+    [
+        'name' => 'New Book Mary',
+        'releaseYear' => '2018',
+        'author' => 'Pravash',
+        'purchaseUrl' => 'http://example.com'
+
+    ],
+];
+
+// function filter($items, $fn)
+// {
+//     $filteredItems = [];
+
+//     foreach ($items as $item) {
+//         if ($fn($item)) {
+//             $filteredItems[] = $item;
+//         }
+//     }
+
+//     return $filteredItems;
+// }
+
+$filteredBooks = array_filter($books, function ($book) {
+    // return $book['releaseYear'] <= '2015';
+    return $book['releaseYear'] >= 2000 && $book['releaseYear'] <= 2020;
+    //  return $book['releaseYear'] === '2015';
+});
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,64 +55,16 @@
 </head>
 
 <body>
-    <?php
-    $books = [
-        [
-            'name' => 'Hail Mary',
-            'releaseYear' => '2015',
-            'author' => 'Pravash',
-            'purchaseUrl' => 'http://example.com'
 
-        ],
-        [
-            'name' => 'Do Androids',
-            'releaseYear' => '2018',
-            'author' => 'Philip',
-            'purchaseUrl' => 'http://example.com'
-
-        ],
-        [
-            'name' => 'New Book Mary',
-            'releaseYear' => '2018',
-            'author' => 'Pravash',
-            'purchaseUrl' => 'http://example.com'
-
-        ],
-    ];
-
-    // function filter($items, $fn)
-    // {
-    //     $filteredItems = [];
-
-    //     foreach ($items as $item) {
-    //         if ($fn($item)) {
-    //             $filteredItems[] = $item;
-    //         }
-    //     }
-
-    //     return $filteredItems;
-    // }
-
-    $filteredBooks = array_filter($books, function ($book) {
-        // return $book['releaseYear'] <= '2015';
-        // return $book['releaseYear'] >= 2000 && $book['releaseYear'] <= 2020;
-        return $book['releaseYear'] === '2015';
-    });
-
-    ?>
-
-    <ul>
-        <?php foreach ($filteredBooks as $book) : ?>
+    <?php foreach ($filteredBooks as $book) : ?>
+        <ul>
             <li>
                 <a href="<?= $book['purchaseUrl'] ?>">
                     <?= $book['name']; ?>
                 </a> by <?= $book['author']; ?>, Released on <?= $book['releaseYear']; ?>
             </li>
-        <?php endforeach; ?>
-    </ul>
-
-
-
+        </ul>
+    <?php endforeach; ?>
 
 </body>
 
