ff2c780041a4f32853fde310620817591124ce29
pravashkarki <kpravash@gmail.com>
2023-01-28 20:23:21 +0545

    learned: functions and filters

--- functions-and-filters.php
+++ functions-and-filters.php
@@ -3,51 +3,9 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>
-        <!DOCTYPE html>
-        <html lang="en">
-
-        <head>
-            <meta charset="UTF-8">
-            <title>Associative Arrays</title>
-        </head>
-
-<body>
-    <?php
-    $books = [
-        [
-            'name' => 'Hail Mary',
-            'relaseDate' => '2015',
-            'author' => 'Pravash',
-            'purchaseUrl' => 'http://example.com'
-
-        ],
-        [
-            'name' => 'Do Androids',
-            'relaseDate' => '2006',
-            'author' => 'Philip',
-            'purchaseUrl' => 'http://example.com'
-
-        ]
-    ];
-    ?>
-
-    <ul>
-        <?php foreach ($books as $book) : ?>
-
-            <li>
-                <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name']; ?>
-                </a> by <?= $book['author']; ?>, Released on <?= $book['relaseDate']; ?>
-            </li>
-
-        <?php endforeach; ?>
-    </ul>
-
-</body>
-
-</html>
-</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functions and Filters</title>
 </head>
 
 <body>
@@ -62,25 +20,47 @@
         ],
         [
             'name' => 'Do Androids',
-            'relaseDate' => '2006',
+            'relaseDate' => '2018',
             'author' => 'Philip',
             'purchaseUrl' => 'http://example.com'
 
-        ]
+        ],
+        [
+            'name' => 'New Book Mary',
+            'relaseDate' => '2018',
+            'author' => 'Pravash',
+            'purchaseUrl' => 'http://example.com'
+
+        ],
     ];
+
+    function filterByAuthor($books, $author)
+    {
+        $filteredBooks = [];
+
+        foreach ($books as $book) {
+            if ($book['author'] === $author) {
+                $filteredBooks[] = $book;
+            }
+        }
+
+        return $filteredBooks;
+    }
+
     ?>
 
     <ul>
-        <?php foreach ($books as $book) : ?>
-
+        <?php foreach (filterByAuthor($books, 'Pravash') as $book) : ?>
             <li>
                 <a href="<?= $book['purchaseUrl'] ?>">
                     <?= $book['name']; ?>
                 </a> by <?= $book['author']; ?>, Released on <?= $book['relaseDate']; ?>
             </li>
-
         <?php endforeach; ?>
     </ul>
+
+
+
 
 </body>
 
