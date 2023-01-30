bb7d98dc223a137ab31a02793d25afd6262bb3dc
pravashkarki <kpravash@gmail.com>
2023-01-30 21:57:11 +0545

    Environments and Configuration Flexibility

--- app/Database.php
+++ app/Database.php
@@ -2,11 +2,14 @@
 class Database
 {
     public $connection;
-    public function __construct()
+    public function __construct($config, $username = 'root', $password = 'root')
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;password=root;charset=utf8mb4";
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+        // $dsn = "mysql:host={$config['host'};port={$config['port'};dbname={$config['dbname'};charset={$config['charset'}";
 
-        $this->connection = new PDO($dsn);
+        $this->connection = new PDO($dsn, $username, $password,[
+          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
     }
     public function query($query)
     {
--- app/index.php
+++ app/index.php
@@ -1,34 +1,14 @@
 <?php
 require('functions.php');
 // require('router.php');
-
 require('Database.php');
 
+$config = require('config.php');
 
-$db = new Database();
+$db = new Database($config['database']);
 $posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
 
 foreach ($posts as $post) {
     echo $post['titile'] . "<br>";
 }
-
-
-
-// class Person
-// {
-//     public $name;
-//     public $age;
-
-//     public function breath()
-//     {
-//         echo $this->name . ' is breathing';
-//     }
-// }
-
-// $person = new Person();
-
-// $person->name = "Pravash Karki";
-// $person->age = 36;
-
-// $person->breath();
