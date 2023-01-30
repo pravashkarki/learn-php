1569710e40e8448af527f4e9c7014be3d228be82
pravashkarki <kpravash@gmail.com>
2023-01-30 14:28:29 +0545

    add: PDO first step

--- app/index.php
+++ app/index.php
@@ -1,3 +1,41 @@
 <?php
 require('functions.php');
-require('router.php');
+// require('router.php');
+
+
+
+// connect to our MySQL database
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;password=root;charset=utf8mb4";
+
+
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo $post['titile'] . "<br>";
+}
+
+
+
+// class Person
+// {
+//     public $name;
+//     public $age;
+
+//     public function breath()
+//     {
+//         echo $this->name . ' is breathing';
+//     }
+// }
+
+// $person = new Person();
+
+// $person->name = "Pravash Karki";
+// $person->age = 36;
+
+// $person->breath();
